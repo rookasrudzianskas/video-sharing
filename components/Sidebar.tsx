efb62e39f4b160ad3d4ae4c76462c77bd0b132a3
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 12:55:30 +0200

    Add sidebar
    - Add sidebar
    - Add sidebar
    - Add sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { ImCancelCircle } from 'react-icons/im';
 
 const Sidebar = ({}) => {
     return (
