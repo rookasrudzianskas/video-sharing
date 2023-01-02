40f0e13f6f82ddc1889bc9b72d10c45ed512fff4
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 12:24:36 +0200

    feat: add isSSR to prevent flash of unstyled content
    This commit adds a isSSR boolean to prevent flash of unstyled content.
    
    This is a feature commit.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {useEffect, useState} from "react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
+
   return (
       <Component {...pageProps} />
   )
