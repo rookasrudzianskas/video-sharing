f231b266dbb2928395a78a4f6de74d8f1f94ad14
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 12:51:03 +0200

    Add logo to navbar
    - Add logo to navbar
    - Add logo to navbar
    - Add logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,10 +7,17 @@ import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import { GoogleLogin, googleLogout  } from '@react-oauth/google';
 
+import { createOrGetUser } from '../utils';
+import Logo from '../utils/tiktik-logo.png';
+
 const Navbar = ({}) => {
     return (
         <div>
-            Navbar
+            <Link href="/">
+                <div>
+                    <Image src={Logo} className="cursor-pointer" alt="Logo" width={50} height={50} />
+                </div>
+            </Link>
         </div>
     );
 };
