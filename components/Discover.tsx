cb10c81940b6aefe76d16aac9d5d25d82388996e
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 14:47:05 +0200

    feat: add popular topics to discover page
    - Add popular topics to discover page
    - Add links to topics

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { topics } from '../utils/constants';
 
 const Discover = ({}) => {
     return (
-        <div>
-
+        <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+            <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">Popular Topics</p>
+            <div className="flex gap-3 flex-wrap">
+                {topics.map((topic) => (
+                    <Link href={`/?topic=${topic.name}`} key={topic.name}>
+                        <div>
+                            <span>
+                                {topic.icon}
+                            </span>
+                            <span>
+                                {topic.name}
+                            </span>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
