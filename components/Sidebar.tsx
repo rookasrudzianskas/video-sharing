00d9f408a469d6a86cdcdb08d9b41d89116cc744
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 12:58:34 +0200

    Fix sidebar icon
    The sidebar icon was not working as expected.
    
    This commit fixes the issue.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,9 +6,14 @@ import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
 
 const Sidebar = ({}) => {
+    const [showSidebar, setShowSidebar] = useState(true);
+
+
     return (
         <div>
-            Sidebar
+            <div className="block xl:hidden m-2 ml-4 mt-3 text-xl" onClick={() => setShowSidebar(!showSidebar)}>
+                {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+            </div>
         </div>
     );
 };
