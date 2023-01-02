3872f1bd6af42d7d78a18ee0d6432ec3fd0fe501
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 14:35:10 +0200

    Add Google Login
    This commit adds Google Login to the sidebar.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,9 +4,12 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
+import GoogleLogin from "react-google-login";
 
 const Sidebar = ({}) => {
     const [showSidebar, setShowSidebar] = useState(true);
+    const userProfile = false;
+    const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded'
 
     return (
         <div>
@@ -17,7 +20,7 @@ const Sidebar = ({}) => {
                 <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
                     <div className="xl:border-b-2 border-gray-200 xl:pb-4">
                         <Link href="/">
-                            <div>
+                            <div className={normalLink}>
                                 <p className="text-2xl">
                                     <AiFillHome />
                                 </p>
@@ -27,6 +30,19 @@ const Sidebar = ({}) => {
                             </div>
                         </Link>
                     </div>
+                    {!userProfile && (
+                        <div className="px-2 py-4 hidden xl:block">
+                            <p className="text-gray-400">Log in to like and comment on videos</p>
+                            <div className="pr-4">
+                                <GoogleLogin
+                                    clientId="YOUR_CLIENT"
+                                    onSuccess={(res) => console.log(res)}
+                                    onFailure={(res) => console.log(res)}
+                                    cookiePolicy={'single_host_origin'}
+                                />
+                            </div>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
