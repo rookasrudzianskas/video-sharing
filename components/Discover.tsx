1c2476d44d46f9cf516dc041e44f5aeb6d944f96
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 17:02:28 +0200

    Add active topic style
    This commit adds a style to the active topic in the discover page.

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -6,6 +6,9 @@ import { useRouter } from 'next/router';
 import { topics } from '../utils/constants';
 
 const Discover = ({}) => {
+    const router = useRouter();
+    const { topic: topicItem } = router.query;
+
     const activeTopicStyle = 'xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]';
     const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
     return (
@@ -14,7 +17,7 @@ const Discover = ({}) => {
             <div className="flex gap-3 flex-wrap">
                 {topics.map((topic) => (
                     <Link href={`/?topic=${topic.name}`} key={topic.name}>
-                        <div className={topicStyle}>
+                        <div className={topicItem === topic.name ? activeTopicStyle : topicStyle}>
                             <span className="font-bold text-2xl xl:text-md">
                                 {topic.icon}
                             </span>
