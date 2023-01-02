24e04ae6b5f193eb502772610db97f183604c913
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 12:48:40 +0200

    Add a search bar and a logout button to the navbar
    - Add a search bar to the navbar
    - Add a logout button to the navbar
    - Add a link to the add page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, googleLogout  } from '@react-oauth/google';
 
 const Navbar = ({}) => {
     return (
