5a1ad2065cf6bff6724f556a3cfe5b6176049aaa
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 12:19:15 +0200

    feat: add a new feature
    This is a new feature that I've been working on.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>TikTok</title>
         <link rel="icon" href="https://cdn.pixabay.com/photo/2021/06/15/12/28/tiktok-6338430_1280.png" />
