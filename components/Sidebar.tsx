6d5fc5633fa2b63c906cd771dabe123a79fdc04b
Rokas <rookas.rudzenskas@gmail.com>
2023-01-02 14:19:47 +0200

    feat: add sidebar
    - add sidebar
    - add home icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,12 +8,27 @@ import { ImCancelCircle } from 'react-icons/im';
 const Sidebar = ({}) => {
     const [showSidebar, setShowSidebar] = useState(true);
 
-
     return (
         <div>
             <div className="block xl:hidden m-2 ml-4 mt-3 text-xl" onClick={() => setShowSidebar((prev) => !prev)}>
                 {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
             </div>
+            {showSidebar && (
+                <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+                    <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+                        <Link href="/">
+                            <div>
+                                <p className="text-2xl">
+                                    <AiFillHome />
+                                </p>
+                                <span className="text-xl hidden xl:block">
+                                    For You
+                                </span>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
