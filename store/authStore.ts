3ea15caf9b2bf9fd251562ec08c8a918f4565824
Rokas <rookas.rudzenskas@gmail.com>
2023-01-03 15:42:19 +0200

    feat: add authStore
    - add authStore
    - add authStore to index

---
+++ store/authStore.ts
@@ -0,0 +1,9 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+import axios from 'axios';
+
+import { BASE_URL } from '../utils';
+
+const authStore = (set: any) => ({
+
+});
