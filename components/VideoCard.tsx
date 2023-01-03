5d98b302a2d75f9030ab1c0b63b9f0ef6383174d
Rokas <rookas.rudzenskas@gmail.com>
2023-01-03 11:52:40 +0200

    Add a newline to the end of VideoCard.tsx
    This is a good practice to follow.

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useRef, useState } from 'react';
 import { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+
 import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import { BsPlay } from 'react-icons/bs';
+
 import {Video} from "../types";
 
 interface IProps {
