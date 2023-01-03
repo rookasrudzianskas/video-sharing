1c4dfd4ad5116f030f523183896c0af569b190c1
Rokas <rookas.rudzenskas@gmail.com>
2023-01-03 12:33:01 +0200

    feat: Add video card
    - Add video card component
    - Add video card styles
    - Add video card tests

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -17,8 +17,52 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, likes }, isShowingOnHome }) => {
     return (
-        <div>
+        <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+            <div className="">
+                <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+                    <div className="md:w-16 md:h-16 w-10 h-10">
+                        <Link href="/profile">
+                            <>
+                                <Image
+                                    width={62}
+                                    height={62}
+                                    className="rounded-full"
+                                    src={postedBy.image}
+                                    alt="Rokas"
+                                />
+                            </>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/">
+                            <div className="flex items-center gap-2">
+                                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                                    {postedBy.userName} {" "}
+                                    <GoVerified className="text-blue-400 text-md" />
+                                </p>
+                                <p className="capitalize text-sm font-medium text-gray-500 hidden md:block">{postedBy.userName}</p>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
 
+            <div className="lg:ml-20 flex gap-4 relative">
+                <div
+                    onMouseEnter={() => {}}
+                    onMouseLeave={() => {}}
+                    className="rounded-3xl">
+                    <Link href="/">
+                        <video
+                            src={video.asset.url}
+                            loop={true}
+                            className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+                        >
+
+                        </video>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
