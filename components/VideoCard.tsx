57c2c4282a01e06fcd48bd8af9406bd2173f16d3
Rokas <rookas.rudzenskas@gmail.com>
2023-01-03 11:50:56 +0200

    feat: add video player
    This commit adds a video player to the video card.
    
    The video player is a custom component that uses the HTML5 video element.
    
    The video player has the following features:
    
    - Play/Pause button
    - Mute/Unmute button
    - Volume slider
    - Progress bar
    - Fullscreen button
    
    The video player also has the following accessibility features:
    
    - Keyboard controls
    - Screen reader support
    
    The video player is tested using Jest and React Testing Library.

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import {NextPage} from "next";
+import React, { useEffect, useRef, useState } from 'react';
+import { NextPage } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
+import { BsPlay } from 'react-icons/bs';
 import {Video} from "../types";
 
 interface IProps {
