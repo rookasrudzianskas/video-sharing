e78374416e33d4e1310bbe94097151c4ed1ea467
Rokas <rookas.rudzenskas@gmail.com>
2023-01-03 14:46:52 +0200

    Fixes #1
    The clientId prop of the GoogleOAuthProvider component was not being passed a string.
    
    This was causing the Google OAuth flow to fail.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if(isSSR) return null;
 
   return (
-      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}>
+      <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
           <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
               <Navbar />
               <div className='flex gap-6 md:gap-20 '>
