4f96b1c1ab4517e280c8e84dc55f2dc329e263e6
Rokas <rookas.rudzenskas@gmail.com>
2023-01-03 15:14:57 +0200

    Add auth api endpoint
    This commit adds an api endpoint to create a user if it doesn't exist.

--- utils/index.ts
+++ utils/index.ts
@@ -3,6 +3,16 @@ import jwt_decode from 'jwt-decode';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const createOrGetUser = async (response: any) => {
-  const decoded = jwt_decode(response.credential);
-  console.log(decoded);
+  const decoded: { name: string, picture: string, sub: string } = jwt_decode(response.credential);
+
+  const { name, picture, sub } = decoded;
+
+  const user = {
+    _id: sub,
+    _type: 'user',
+    userName: name,
+    image: picture
+  };
+
+  await axios.post(`http://localhost:3000/api/auth`, user);
 };
