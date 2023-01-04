1e8649f6193ab1a9f52d4d64061571f474dbdf09
Rokas <rookas.rudzenskas@gmail.com>
2023-01-04 19:41:50 +0200

    Add a new feature
    This is a new feature that I have added to the project.

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,4 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsFillPlayFill } from 'react-icons/bs';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+
+
+// import Comments from '../../components/Comments';
+import { BASE_URL } from '../../utils';
+// import LikeButton from '../../components/LikeButton';
+import useAuthStore from '../../store/authStore';
+import { Video } from '../../types';
+import axios from 'axios';
+
+interface IProps {
+    postDetails: Video;
+}
 
 const Detail = ({}) => {
     return (
