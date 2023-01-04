7b863b1fb3ff193deb0a432af6d5e7dbcec3eb6c
Rokas <rookas.rudzenskas@gmail.com>
2023-01-04 10:44:13 +0200

    feat: add user to store
    This commit adds the user to the store.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,9 +9,12 @@ import { GoogleLogin, googleLogout  } from '@react-oauth/google';
 
 import { createOrGetUser } from '../utils';
 import Logo from '../utils/tiktik-logo.png';
+import useAuthStore from '../store/authStore';
 
 const Navbar = ({}) => {
     const user = false;
+    const { userProfile, addUser, removeUser } = useAuthStore();
+
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
             <Link href="/">
@@ -38,7 +41,7 @@ const Navbar = ({}) => {
                 ) : (
                     <GoogleLogin
                         onError={() => console.log('Login Failed')}
-                        onSuccess={(response) => createOrGetUser(response)}
+                        onSuccess={(response) => createOrGetUser(response, addUser)}
                     />
                 )}
             </div>
--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const createOrGetUser = async (response: any) => {
+export const createOrGetUser = async (response: any, addUser: any) => {
   const decoded: { name: string, picture: string, sub: string } = jwt_decode(response.credential);
 
   const { name, picture, sub } = decoded;
@@ -14,7 +14,7 @@ export const createOrGetUser = async (response: any) => {
     image: picture
   };
 
-  // addUser(user);
+  addUser(user);
 
   await axios.post(`http://localhost:3000/api/auth`, user);
 };
