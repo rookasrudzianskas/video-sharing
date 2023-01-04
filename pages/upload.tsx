8f9c92507df544e64ab704342f7d1b91ac0b492b
Rokas <rookas.rudzenskas@gmail.com>
2023-01-04 15:33:55 +0200

    feat: add upload video feature
    - add upload video feature

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -10,9 +10,36 @@ import { BASE_URL } from '../utils';
 import { client } from '../utils/client';
 import { topics } from '../utils/constants';
 const Upload = ({}) => {
-    return (
-        <div>
+    const [isLoading, setIsLoading] = useState<Boolean>(false);
+    const [videoAsset, setVideoAsset] = useState();
 
+    return (
+        <div className="flex w-full h-full">
+            <div className="bg-white rounded-lg">
+                <div>
+                    <div>
+                        <p className="text-2xl font-bold">Upload Video</p>
+                        <p className="text-md text-gray-400 mt-1">Post a video to your account</p>
+                    </div>
+                    <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+                        {isLoading ? (
+                            <p>Uploading...</p>
+                        ) : (
+                            <div>
+                                {videoAsset ? (
+                                    <div>
+
+                                    </div>
+                                ) : (
+                                    <label htmlFor="">
+
+                                    </label>
+                                )}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
