fc75860f8b89ace8662e3669b0eab645a86d5d59
Rokas <rookas.rudzenskas@gmail.com>
2023-01-04 16:23:25 +0200

    Add a button to post the image
    This button will be used to post the image to the server.

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -40,6 +40,10 @@ const Upload = ({}) => {
         }
     }
 
+    const handlePost = async () => {
+
+    }
+
     return (
         <div className='flex w-full h-full absolute left-0 top-[60px] lg:top-[70px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center'>
             <div className=' bg-white rounded-lg xl:h-[80vh] flex gap-6 flex-wrap justify-center items-center p-14 pt-6'>
