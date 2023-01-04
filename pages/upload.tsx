fdaa9486105dc5de800ead91f069b62b432200c6
Rokas <rookas.rudzenskas@gmail.com>
2023-01-04 15:22:41 +0200

    feat: add upload page
    This is the first step towards adding the upload page.

---
+++ pages/upload.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { SanityAssetDocument } from '@sanity/client';
+import { useRouter } from 'next/router';
+import { FaCloudUploadAlt } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
+import axios from 'axios';
+
+import useAuthStore from '../store/authStore';
+import { BASE_URL } from '../utils';
+import { client } from '../utils/client';
+import { topics } from '../utils/constants';
+const Upload = ({}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Upload;
+// by Rokas with ❤️
