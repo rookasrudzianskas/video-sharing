00685a4db6567fddcab06bc61af362889db6783b
Rokas <rookas.rudzenskas@gmail.com>
2023-01-04 16:31:01 +0200

    Add status code to response
    This is a test commit message.

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const doc = req.body;
 
         client.create(doc).then(() => {
-            res.status(200).json('Success');
+            res.status(201).json('Success');
         });
     }
 }
