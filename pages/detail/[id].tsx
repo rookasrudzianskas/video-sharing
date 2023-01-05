32a32fc6fc1936e5577fc34772d6c8805d4c54ae
Rokas <rookas.rudzenskas@gmail.com>
2023-01-05 12:21:24 +0200

    feat: add background image to detail page
    The background image is a blurred version of the video thumbnail.
    
    This is a breaking change because the background image is now a background
    image instead of a background color.

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -33,7 +33,7 @@ const Detail = ({ postDetails }: IProps) => {
 
     return (
         <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
-            <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img">
+            <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
                 <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
                     <p>
                         <MdOutlineCancel className="text-white text-[35px]" />
@@ -42,6 +42,7 @@ const Detail = ({ postDetails }: IProps) => {
                 <div className="relative">
                     <div className="lg:h-[100vh] h-[60vh]">
                         <video
+                            className="h-full cursor-pointer"
                             src={post.video.asset.url}
                         >
 
