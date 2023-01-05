b0da41c2c6299914a581cb102f7fabea58c76acc
Rokas <rookas.rudzenskas@gmail.com>
2023-01-05 12:20:32 +0200

    Add video player
    - Add video player
    - Add video controls
    - Add video mute button
    - Add video play/pause button
    - Add video progress bar
    - Add video fullscreen button
    - Add video comment button
    - Add video comment input
    - Add video comment submit button
    - Add video comment list
    - Add video comment list item
    - Add video comment list item avatar
    - Add video comment list item username
    - Add video comment list item comment
    - Add video comment list item timestamp

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -21,10 +21,34 @@ interface IProps {
 
 const Detail = ({ postDetails }: IProps) => {
     const [post, setPost] = useState(postDetails);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const [isVideoMuted, setIsVideoMuted] = useState<boolean>(false);
+    const [isPostingComment, setIsPostingComment] = useState<boolean>(false);
+    const [comment, setComment] = useState<string>('');
+
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const router = useRouter();
+
+    if(!post) return null;
 
     return (
-        <div>
-            Hello
+        <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+            <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img">
+                <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+                    <p>
+                        <MdOutlineCancel className="text-white text-[35px]" />
+                    </p>
+                </div>
+                <div className="relative">
+                    <div className="lg:h-[100vh] h-[60vh]">
+                        <video
+                            src={post.video.asset.url}
+                        >
+
+                        </video>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
