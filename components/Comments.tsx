e3a2e9db162402d33e68391b6fa6c6a79c009e65
Rokas <rookas.rudzenskas@gmail.com>
2023-01-05 13:35:49 +0200

    Add comments to the post
    - Add comments to the post
    - Add comments to the post
    - Add comments to the post

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
 
+import useAuthStore from '../store/authStore';
+import NoResults from './NoResults';
+import { IUser } from '../types';
+
+interface IProps {
+    isPostingComment: boolean;
+    comment: string;
+    setComment: Dispatch<SetStateAction<string>>;
+    addComment: (e: React.FormEvent) => void;
+    comments: IComment[];
+}
+
+interface IComment {
+    comment: string;
+    length?: number;
+    _key: string;
+    postedBy: { _ref?: string; _id?: string };
+}
 const Comments = ({}) => {
     return (
         <div>
-            
+
         </div>
     );
 };
