18333c9e7cbba205279b5f02adca2227994a736c
Rokas <rookas.rudzenskas@gmail.com>
2023-01-06 15:04:07 +0200

    feat: add search page
    This commit adds a search page that allows users to search for videos.
    
    The search page is a Next.js dynamic page that takes a search term as a parameter.
    
    The search page uses the search API endpoint to fetch videos that match the search term.
    
    The search page displays the videos in a grid.

---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Link from 'next/link';
+import axios from 'axios';
+
+import NoResults from '../../components/NoResults';
+import VideoCard from '../../components/VideoCard';
+import useAuthStore from '../../store/authStore';
+import { BASE_URL } from '../../utils';
+import { IUser, Video } from '../../types';
+
+const Search = ({}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Search;
+// by Rokas with ❤️
