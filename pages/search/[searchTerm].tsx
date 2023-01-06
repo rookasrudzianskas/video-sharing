64f7c300a64ba21187d4508ff13010331436f6a5
Rokas <rookas.rudzenskas@gmail.com>
2023-01-06 15:58:34 +0200

    feat: add search page
    - Added search page
    - Added search bar
    - Added search results

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -12,9 +12,25 @@ import { BASE_URL } from '../../utils';
 import { IUser, Video } from '../../types';
 
 const Search = ({videos}: { videos: Video[] }) => {
-    return (
-        <div>
+    const [isAccounts, setIsAccounts] = useState(false);
+    const { allUsers }: { allUsers: IUser[] } = useAuthStore();
 
+    const router = useRouter();
+    const { searchTerm }: any = router.query;
+
+    const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+    const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+
+    return (
+        <div className="w-full">
+            <div className='flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full'>
+                <p onClick={() => setIsAccounts(true)} className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}>
+                    Accounts
+                </p>
+                <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
+                    Videos
+                </p>
+            </div>
         </div>
     );
 };
