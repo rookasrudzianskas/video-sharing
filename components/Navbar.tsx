29b16fce928997131fd6d961054a746bbddb7b1c
Rokas <rookas.rudzenskas@gmail.com>
2023-01-06 14:59:45 +0200

    Add search icon to search bar
    The search icon is added to the search bar to make it more obvious that the
    search bar is a search bar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = ({}) => {
                     <input
                         value={searchValue}
                         onChange={(e) => setSearchValue(e.target.value)}
-                        className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0'
+                        className='bg-primary p-3 pl-5 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0'
                         placeholder='Search accounts and videos'
                     />
                 </form>
