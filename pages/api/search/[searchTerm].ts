2b5a3613600f855582797edd1ac6ddf82c8e96d7
Rokas <rookas.rudzenskas@gmail.com>
2023-01-06 15:55:18 +0200

    feat: add search functionality
    - add search functionality
    - add search query
    - add search route

--- pages/api/search/[searchTerm].ts
+++ pages/api/search/[searchTerm].ts
@@ -5,9 +5,9 @@ import { client } from '../../../utils/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === 'GET') {
-        const { id } = req.query;
+        const { searchTerm } = req.query;
 
-        const videosQuery = searchPostsQuery(id);
+        const videosQuery = searchPostsQuery(searchTerm);
         const videos = await client.fetch(videosQuery);
 
         res.status(200).json(videos);
